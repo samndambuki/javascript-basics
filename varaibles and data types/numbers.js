6cb7a5e128fea90e0734280ec31ffe14af06f88f
samndambuki <sam.ndambuki08@gmail.com>
2023-02-15 11:48:54 +0300

    Numbers

---
+++ varaibles and data types/numbers.js
@@ -0,0 +1,12 @@
+// 12
+// 12.0
+// -12
+// Infinity
+// NaN - Not a number
+var myNumber = 33
+
+console.log(myNumber)
+var x = Math.round(12.122221233442)
+console.log(x)
+z = Math.random()
+console.log(z)
