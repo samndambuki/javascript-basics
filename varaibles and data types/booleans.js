aed86352112fee137f847803be29aa465868bd4e
samndambuki <sam.ndambuki08@gmail.com>
2023-02-15 11:54:08 +0300

    booleans

---
+++ varaibles and data types/booleans.js
@@ -0,0 +1,8 @@
+// true
+// false
+var buttonHasBeenClicked = false;
+console.log(buttonHasBeenClicked)
+var myLocation = "Nyeri", myOtherLocation = "Nairobi"
+myOtherLocation = "Nyeri"
+var x = myLocation === myOtherLocation //comparisons
+console.log(x)
