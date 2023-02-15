22107ccda152fbfdbc760450dbe5c58e7bad8079
samndambuki <sam.ndambuki08@gmail.com>
2023-02-15 13:08:17 +0300

    arrays

---
+++ Objects/arrays.js
@@ -0,0 +1,18 @@
+var myArray = [];
+console.log(myArray)
+var daysOfTheWeek = ['Sunday','Monday','Tuesday'];
+console.log(daysOfTheWeek);
+myArray = [0,1,2,'string1','string2','string3',true,false];
+console.log(myArray)
+var counties = ["Nyeri","Nairobi","Machakos","Kakamega"]
+console.log(counties)
+var arrayOfStuff = [
+    {'name':'value'},
+    [1,2,3],
+    true,
+    'Sam'
+]
+console.log(arrayOfStuff)
+x = arrayOfStuff.length
+console.log(x)
+
