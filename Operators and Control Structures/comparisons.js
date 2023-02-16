a7a3ceeaa32f450413940dbfde59511e56d25c7a
samndambuki <sam.ndambuki08@gmail.com>
2023-02-16 09:34:42 +0300

    simple comparisons

---
+++ Operators and Control Structures/comparisons.js
@@ -0,0 +1,15 @@
+var one = 1, two = 2;
+x = one === one //equaity operator
+y = one !==one
+z = one != two
+
+one == one
+one == "1"
+one != "1"
+
+one < two
+one > two
+
+console.log(x)
+console.log(y)
+console.log(z)
