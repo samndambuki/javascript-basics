db7d4d3fc7e341a1e39f4b141202b6c50118f631
samndambuki <sam.ndambuki08@gmail.com>
2023-02-16 10:18:30 +0300

    conditionals if

---
+++ Operators and Control Structures/if.js
@@ -0,0 +1,7 @@
+// control structure - modify flow of execution
+//if - branch your code depending on certain decissions
+
+
+var answer = window.confirm("Click OK, get true. Click cancel, get false");
+answer;
+
