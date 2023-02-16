ca672cafe9c8cb52174dbad5bbfc030a7db5299d
samndambuki <sam.ndambuki08@gmail.com>
2023-02-16 09:44:10 +0300

    arithmetic operators

---
+++ Operators and Control Structures/arithmetic.js
@@ -0,0 +1,20 @@
+20 % 2 // modulus operator
+19 % 2 
+
+//incrementing and decrementing
+var counter = 0 
+x = counter = counter + 1
+y = counter += 1
+z = counter ++
+a = counter += 5
+
+counter *= 2 //double it 
+
+c= "cat" + "dog"
+console.log(c)
+
+console.log(x)
+console.log(y)
+console.log(z) 
+console.log(a)
+
