25c29e6c60e439e34c3ca26063f988ddf1e190e6
samndambuki <sam.ndambuki08@gmail.com>
2023-02-16 09:53:12 +0300

    logical operators

---
+++ Operators and Control Structures/logical.js
@@ -0,0 +1,15 @@
+//and &&
+//or ||
+
+
+var animal1 = "monkey";
+var animal2 = "bear";
+var animal3 = "tiger";
+
+// x = animal1 === 'ape' && animal2 === 'bear' && animal3 === 'tiger'
+//!true
+//!false
+!(animal1 === "monkey" && animal2 === "bear")
+animal1 !== "monkey" || animal2 !== "bear"
+console.log(x)
+
