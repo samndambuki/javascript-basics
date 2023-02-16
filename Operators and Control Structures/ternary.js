19c21b7e01f72b231d77c370c2d5e9988501e6db
samndambuki <sam.ndambuki08@gmail.com>
2023-02-16 10:47:13 +0300

    ternary operator

---
+++ Operators and Control Structures/ternary.js
@@ -0,0 +1,18 @@
+//ternary or conditional operator
+// var animal = "cat";
+// animal = "dog";
+
+animal === "cat"
+?console.log("You will be a cat herder")
+:console.log("You will be a dog catcher");
+
+var job = animal === "cat"
+?console.log("You will be a cat herder")
+:console.log("You will be a dog catcher");
+
+animal = 'dog'
+var job = animal === "cat"
+?console.log("You will be a cat herder")
+:console.log("You will be a dog catcher");
+
+console.log(job)
