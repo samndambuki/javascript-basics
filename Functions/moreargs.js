dbb0ef325e69b75f27d57eef9c3583413a2b2f91
samndambuki <sam.ndambuki08@gmail.com>
2023-02-23 09:42:15 +0300

    more on arguments

--- Functions/moreargs.js
+++ Functions/moreargs.js
@@ -1,18 +1,14 @@
-function speakSomething(what, howMany) {
-  // this pattern works nicely for default values:
-  // check if the argument is undefined, and if it is, provide a default value
-  var what = (typeof what !== "undefined") ? what : "Default speech";
-  var howMany = (typeof howMany !== "undefined") ? howMany : 10;
+// speakSomething('hey hey', 5)
+// speakSomething('hey hey')
 
-  // shorter version that could get tripped up by truthiness
-  // what = what || 'Default speech';
-  // howMany = howMany || 10;
+// function speakSomething(what, howMany){
+//   var what = (typeof what !== "undefined") ? what: "Default speech";
+//   var howMany = (typeof howMany !== "undefined") ? howMany :10;
 
-  for (var i = 0; i < howMany; i += 1) {
-    console.log(what + " (" + i + ")");
-  }
-}
+//   for (var i=0;i<howMany;i+=1){
+//     console.log(what + "(" + i + ")");
+//   }
+// }
 
-speakSomething("Hey hey", 5);
-speakSomething("Hey hey");
-speakSomething();
+
+addingMachine(1,2,3)
